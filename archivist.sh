34d26fcc85a5fdaec24c71982b0c02f649f96c88
Filip Sądowicz <sadowicz31@gmail.com>
2020-12-28 20:04:22 +0100

    Adding error prompting function

--- archivist.sh
+++ archivist.sh
@@ -12,17 +12,24 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 
-# functions definitions
-
 # script program variables
 
 argv=("$@")
 supported_patterns='tar|gzip|bzip2|zip|xz'
 
+declare -A COLORS=([RED]="\033[1;31m" [UNSET]="\033[0m")
+
+# functions definitions
+
+function err() {
+
+	echo -e "${COLORS[RED]}${__base} : ERROR:${COLORS[UNSET]} $1" >&2
+}
+
 # main script
 
-format="$(egrep --only-matching --word-regexp ${supported_patterns} <<< \
-		"$(file ${argv[0]} | tr '[:upper:]' '[:lower:]')" | head --lines=1 )"
+format="$(echo "$(egrep --only-matching --word-regexp ${supported_patterns} <<< \
+		"$(file ${argv[0]} | tr '[:upper:]' '[:lower:]')" | head --lines=1)")"
 
 case ${format} in
 	"tar")
@@ -43,6 +50,6 @@ case ${format} in
 		unxz "${argv[0]}.xz"
 		;;
 	*)
-		echo "ERROR: unsupported archive type" >$2
+		err "Unsupported archive type"
 		;;
 esac
