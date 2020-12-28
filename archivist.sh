90af1b5875a6d1ac91450c566726ae2b709f6eee
Filip Sądowicz <sadowicz31@gmail.com>
2020-12-28 14:58:27 +0100

    Adding zip and xz decompression

--- archivist.sh
+++ archivist.sh
@@ -17,22 +17,30 @@ __base="$(basename ${__file} .sh)"
 # script program variables
 
 argv=("$@")
-supported_pattern='tar|gzip|bzip2'
+supported_patterns='tar|gzip|bzip2|zip|xz'
 
 # main script
 
-format="$(egrep --only-matching --word-regexp ${supported_pattern} <<< "$(file ${argv[0]})" |
-		head --lines=1)"
+format="$(egrep --only-matching --word-regexp ${supported_patterns} <<< \
+		"$(file ${argv[0]} | tr '[:upper:]' '[:lower:]')" | head --lines=1 )"
 
 case ${format} in
 	"tar")
-		tar -xf ${argv[0]}
+		tar -xf "${argv[0]}"
 		;;
 	"gzip")
-		gunzip ${argv[0]}
+		gunzip "${argv[0]}"
 		;;
 	"bzip2")
-		bunzip2 ${argv[0]}
+		bunzip2 "${argv[0]}"
+		;;
+	"zip")
+		unzip "${argv[0]}"
+		;;
+	"xz")
+		cp "${argv[0]}" "${argv[0]}.xz"
+		rm "${argv[0]}"
+		unxz "${argv[0]}.xz"
 		;;
 	*)
 		echo "ERROR: unsupported archive type" >$2
