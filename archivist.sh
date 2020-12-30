44855ec8834128dc269ecdf8e23cda65837a5a6a
Filip Sądowicz <sadowicz31@gmail.com>
2020-12-30 17:49:58 +0100

    Adding program's positional arguments parsing

--- archivist.sh
+++ archivist.sh
@@ -14,7 +14,6 @@ __base="$(basename ${__file} .sh)"
 
 # script program variables
 
-argv=("$@")
 supported_patterns='tar|gzip|bzip2|zip|xz'
 
 declare -A COLORS=([RED]="\033[1;31m" [UNSET]="\033[0m")
@@ -24,30 +23,101 @@ declare -A COLORS=([RED]="\033[1;31m" [UNSET]="\033[0m")
 function err() {
 
 	echo -e "${COLORS[RED]}${__base} : ERROR:${COLORS[UNSET]} $1" >&2
+	exit 1
+}
+
+function get_args() {
+
+	# Long options to short
+
+	for arg in "$@"; do
+		shift
+		case "${arg}" in
+			"--create")
+			set -- "$@" "-c"
+			;;
+			"--extract")
+			set -- "$@" "-x"
+			;;
+			"--verbose")
+			set -- "$@" "-v"
+			;;
+			"--help")
+			set -- "$@" "-h"
+			;;
+			"--"*)
+			err "Invalid program argument ${arg}.\t\n\tCheck ${__base} --help for more info on usage."
+			;;
+			*)
+			set -- "$@" "${arg}"
+			;;
+		esac
+	done
+
+	# Set defaults
+
+	CREATE=false
+	EXTRACT=false
+	VERBOSE=false
+	HELP=false
+	FILE=""
+
+	# Parse short options
+	
+	OPTIND=1
+	while getopts ":cxvh" opt; do
+		case "${opt}" in
+			"c")
+			CREATE=true
+			;;
+			"x")
+			EXTRACT=true
+			;;
+			"v")
+			VERBOSE=true
+			;;
+			"h")
+			HELP=true
+			;;
+			"?")
+			err "Invalid program argument -${OPTARG}.\t\n\tCheck ${__base} --help for more info on usage."
+			;;
+
+		esac
+	done
+	
+	# Remove optins from positional parmeters
+
+	shift "$((OPTIND-1))"
+
+	FILE=("$@")
+	FILE=${FILE[0]}
 }
 
 # main script
 
+get_args "$@"
+
 format="$(echo "$(egrep --only-matching --word-regexp ${supported_patterns} <<< \
-		"$(file ${argv[0]} | tr '[:upper:]' '[:lower:]')" | head --lines=1)")"
+		"$(file ${FILE} | tr '[:upper:]' '[:lower:]')" | head --lines=1)")"
 
 case ${format} in
 	"tar")
-		tar -xf "${argv[0]}"
+		tar -xf "${FILE}"
 		;;
 	"gzip")
-		gunzip "${argv[0]}"
+		gunzip "${FILE}"
 		;;
 	"bzip2")
-		bunzip2 "${argv[0]}"
+		bunzip2 "${FILE}"
 		;;
 	"zip")
-		unzip "${argv[0]}"
+		unzip "${FILE}"
 		;;
 	"xz")
-		cp "${argv[0]}" "${argv[0]}.xz"
-		rm "${argv[0]}"
-		unxz "${argv[0]}.xz"
+		cp "${FILE}" "${FILE}.xz"
+		rm "${FILE}"
+		unxz "${FILE}.xz"
 		;;
 	*)
 		err "Unsupported archive type"
